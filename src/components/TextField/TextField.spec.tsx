fc2383984e30aea12b92bd0c9a67ffec88fa4d40
Vinicius <beneditoloura@gmail.com>
2024-06-14 06:47:25 -0300

    refactor: change textField file name and finish component

---
+++ src/components/TextField/TextField.spec.tsx
@@ -0,0 +1,47 @@
+// TextField.test.tsx
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import TextField from './TextField'
+
+describe('TextField Component', () => {
+  const handleConfirm = jest.fn()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should render the input field with correct placeholder', () => {
+    render(<TextField handleConfirm={handleConfirm} />)
+
+    const inputElement = screen.getByPlaceholderText('Digite o nome do jogador')
+    expect(inputElement).toBeInTheDocument()
+  })
+
+  test('Should calls handleConfirm function on button click', async () => {
+    render(<TextField handleConfirm={handleConfirm} />)
+
+    const inputElement = screen.getByPlaceholderText('Digite o nome do jogador')
+    const confirmButton = screen.getByTestId('confirm-button')
+    const newValue = 'New Value'
+
+    await userEvent.type(inputElement, newValue)
+    await userEvent.click(confirmButton)
+
+    expect(handleConfirm).toHaveBeenCalledWith({ value: newValue })
+    expect(inputElement).toHaveValue(newValue)
+  })
+
+  test('Should not calls handleConfirm function when click on the button but input is empty', async () => {
+    render(<TextField handleConfirm={handleConfirm} />)
+
+    const confirmButton = screen.getByTestId('confirm-button')
+
+    screen.logTestingPlaygroundURL()
+
+    await userEvent.click(confirmButton)
+
+    expect(handleConfirm).not.toHaveBeenCalled()
+  })
+})
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { TextFieldProps } from './types'
+import IconSearch from '../icons/SearchIcon'
+
+const TextField = ({ handleConfirm }: TextFieldProps) => {
+  const [value, setValue] = useState('')
+
+  const shouldCallHandleConfirmFunction = !!value.trim()
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Digite o nome do jogador"
+        className="p-2 mr-3 text-black"
+      />
+
+      <button
+        type="button"
+        aria-label="Pesquisar Jogador"
+        onClick={() =>
+          shouldCallHandleConfirmFunction && handleConfirm({ value })
+        }
+        data-testid="confirm-button"
+        className="bg-white p-3"
+      >
+        <IconSearch />
+      </button>
+    </div>
+  )
+}
+
+export default TextField
