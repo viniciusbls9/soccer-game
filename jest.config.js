b95e7a9ca828c5918c523e7a5e6ea95519849ab8
Vinicius <beneditoloura@gmail.com>
2024-06-14 06:46:54 -0300

    chore: jest and RTL configs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
-  transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
-  },
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
