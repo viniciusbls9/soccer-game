352acd36503114bfb3c51b211b2ac265b48210b8
Vinicius <beneditoloura@gmail.com>
2024-06-20 18:37:20 -0300

    feat: create PlayerInfoCardProps interface

---
+++ src/components/PlayerInfoCard/types.ts
@@ -0,0 +1,11 @@
+export interface PlayerInfoCardProps {
+  id: string
+  name: string
+  position: string
+  selection: string
+  club: string
+  goals: number
+  assists: number
+  age: number
+  photo: string
+}
